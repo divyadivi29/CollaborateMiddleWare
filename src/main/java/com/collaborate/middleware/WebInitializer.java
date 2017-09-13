289750f9c163916d2888f22829126197161b19c2
SONY <SONY@192.168.0.5>
2017-09-13 22:14:45 +0530

    WebREsolver

--- src/main/java/com/collaborate/middleware/WebInitializer.java
+++ src/main/java/com/collaborate/middleware/WebInitializer.java
@@ -2,21 +2,21 @@ package com.collaborate.middleware;
 
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer
+public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer 
 {
-@Override
-protected class<?>[] getRootConfigClasses()
-{
-	return new Class[]{WebResolver.class};
-}
-@Override
-protected class<?>[] getServletConfigClasses()
-{
-	return null;
-}
-@Override
-protected String[] getServletMapping()
-{
-	return new String[] {"/"};
-}
-}
+		@Override
+		protected Class<?>[] getRootConfigClasses()
+		{
+			return new Class[]{WebResolver.class};
+		}
+		@Override
+		protected Class<?>[] getServletConfigClasses()
+		{
+			return null;
+		}
+		@Override
+		protected String[] getServletMappings()
+		{
+			return new String[]{"/"};
+		}
+	}
--- src/main/java/com/collaborate/middleware/WebResolver.java
+++ src/main/java/com/collaborate/middleware/WebResolver.java
@@ -1,13 +1,23 @@
 package com.collaborate.middleware;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("com.collaborate")
-
-public class WebResolver {
-
+public class WebResolver
+{
+ @Bean
+ public InternalResourceViewResolver getViewResolver()
+ {
+    InternalResourceViewResolver internalResourceViewResolver=new InternalResourceViewResolver();
+    internalResourceViewResolver.setPrefix("/WEB-INF/");
+    internalResourceViewResolver.setSuffix(".jsp");
+    System.out.println("Resolver Created");
+    return internalResourceViewResolver;
+}
 }
